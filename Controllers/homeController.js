4f64b30717794fd8f77ee1fdbcb0d886feb36d81
Cheema Mahesh <maheshbabucheema789@gmail.com>
2023-10-03 00:13:02 +0530

    CREATE A POST 2.0

--- Controllers/homeController.js
+++ Controllers/homeController.js
@@ -1,11 +1,17 @@
 const User=require('../models/user');
+const Post=require('../models/posts');
 
 module.exports.homePage=async function(req,res){
     try{
         if(!req.isAuthenticated()){
             return res.redirect('/auth/login');
           }
-        return res.render('home');
+          const posts = await Post.find({})
+          .sort('-createdAT')
+          .exec();
+        return res.render('home',{
+            posts
+        });
     }catch(err){
         console.log("error in rendring the home page",err);
         return res.status(500);
